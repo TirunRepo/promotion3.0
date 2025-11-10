6d72bd676fc7eee25ea42d8be9b2147dc62076dc
User <User@DESKTOP-GM9J61D>
2025-11-10 13:44:44 +0530

    update upload-image-endpoint relation with inventory

--- MarketPlace_Backend/MarketPlace.Business/Repositories/Inventory/Respository/CruiseDeckImageRepository.cs
+++ MarketPlace_Backend/MarketPlace.Business/Repositories/Inventory/Respository/CruiseDeckImageRepository.cs
@@ -1,17 +1,31 @@
 ﻿using MarketPlace.Business.Repositories.Inventory.Interface;
 using MarketPlace.DataAccess.DBContext;
 using MarketPlace.DataAccess.Entities.Inventory;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class CruiseDeckImageRepository : ICruiseDeckImageRepository
 {
     private readonly AppDbContext _context;
-    public CruiseDeckImageRepository(AppDbContext context) { _context = context; }
 
-    public async Task AddRangeAsync(List<CruiseDeckImage> deckImages)
+    public CruiseDeckImageRepository(AppDbContext context)
     {
-        await _context.CruiseDeckImages.AddRangeAsync(deckImages);
+        _context = context;
+    }
+
+    public async Task AddAsync(CruiseDeckImage entity)
+    {
+        await _context.CruiseDeckImages.AddAsync(entity);
+    }
+
+    public async Task SaveChangesAsync()
+    {
         await _context.SaveChangesAsync();
+    }
+
+    public async Task<CruiseInventory?> GetByIdAsync(int id)
+    {
+        return await _context.CruiseInventories.FirstOrDefaultAsync(x => x.Id == id);
     }
 }
